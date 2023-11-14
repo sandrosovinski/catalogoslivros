c915f0948accb3de8a39f7825d64ce6d8aa001ca
sandrosovinski <ssovinski@gmail.com>
2023-11-14 17:48:59 -0300

    Alterações realizadas em 14/11/2023

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 from flask_wtf import FlaskForm 
 from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
@@ -8,34 +9,24 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key = 'djhfjdhfdkfheirweuryeuryei'
 
-#messages = [{'titulo': 'Mensagem Titulo',
-#             'autor': 'Mensagem Autor',
-#             'exemplares': 'Mensagem Exemplares'}
-#            ] 
-
-# adding configuration for using a sqlite database
+# Configurações do Banco de Dads SQLITE
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///catalogoslivros.db'
   
-# Creating an SQLAlchemy instance
+#Criando Instância SQLAlchemy
 db = SQLAlchemy(app)
  
-# Settings for migrations
-#migrate = Migrate(app, db)
-
-# Models
+# Modelos da tabela catalogoslivros
 class Catalogoslivros(db.Model):
-    # Id : Field which stores unique id for every row in 
-    # database table.
-    # first_name: Used to store the first name if the user
-    # last_name: Used to store last name of the user
-    # Age: Used to store the age of the user
+    # id: Identificado Único na tabela catalogoslivros
+    # titulo: Usado para definir o campo Titulo do Livro
+    # autor: Usado para definir o campo Autor do Livro
+    # exemplares: Usado para definir o número de Exemplares disponíveis
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(255), unique=False, nullable=False)
     autor = db.Column(db.String(255), unique=False, nullable=False)
     exemplares = db.Column(db.Integer, nullable=False)
-  
-    # repr method repersents how one object of this datatable
-    # will look like
+
+    #Método repr que representa a aparência de um objeto da Tabela catalogoslivros
     def __repr__(self):
         return f"Titulo do Livro : {self.titulo}, Autor do Livro: {self.autor}, Exemplares: {self.exemplares}"
 
@@ -43,10 +34,6 @@ class Catalogoslivros(db.Model):
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/home")
-def home():
-    return render_template("home.html")
 
 #Pagina para acesso ao aplicativo
 @app.route("/acesso")
@@ -58,6 +45,7 @@ def acesso():
 def sobre():
     return render_template("sobre.html")
 
+#Página para informação de operação realizada com sucesso
 @app.route("/sucesso")
 def sucesso():
     return render_template("sucesso.html")
@@ -69,60 +57,77 @@ class LivroForm(FlaskForm):
     exemplares = IntegerField('Exemplares')
     submit = SubmitField('Enviar')
 
-# function to add profiles
+# Função para cadstrar um novo Livro
 @app.route("/cadastrar", methods=['GET','POST'])
 def cadastrar():
+    #Cria uma nova intância da clase LivroForm
     form = LivroForm()
-
-    #titulo = request.form.get("titulo")
-    #autor = request.form.get("autor")
-    #exemplares = request.form.get("exemplares")
-    titulo = form.titulo.data
-    autor  = form.autor.data
-    exemplares = form.exemplares.data
-  
-    # create an object of the Profile class of models
-    # and store data as a row in our datatable
     if request.method == 'POST':
-        if form.validate_on_submit():
-            if titulo != '' and autor != '' and exemplares is not None:
-                p = Catalogoslivros(titulo=titulo, autor=autor, exemplares=exemplares)
-                db.session.add(p)
-                db.session.commit()
-                #messages.append({'titulo': titulo, 'autor': autor, 'exemplares': exemplares})
-                return render_template('sucesso.html', form=form)
-            #else:
-            #    return redirect('cadastrar.html', form=form)
+        titulo = request.form.get("titulo")
+        autor = request.form.get("autor")
+        exemplares = request.form.get("exemplares")
+
+        #Verifica se o número de exemplares é igual ou menos que 0
+        if int(exemplares) <= 0:
+            return render_template('erro.html', form=form)
+    
+    if form.validate_on_submit():
+        if titulo != '' and autor != '' or exemplares is not None:
+            p = Catalogoslivros(titulo=titulo, autor=autor, exemplares=exemplares)
+            db.session.add(p)
+            db.session.commit()
+            db.session.close()
+            return render_template('sucesso.html', form=form)
+        else:
+            return render_template('erro.html', form=form)
    
     return render_template('cadastrar.html', form=form)
 
-# function to render index page
+# Função para listar tidos os livros cadastrado na base de daods e sua disponibilidade
 @app.route('/disponibilidade')
 def disponibilidade():
     page = request.args.get('page', 1, type=int)
     pagination = Catalogoslivros.query.order_by(Catalogoslivros.titulo).paginate(page=page, per_page=4)
-    #catalogoslivros = Catalogoslivros.query.all()
-    #return render_template('index.html', catalagoslivros=catalogoslivros,posts=posts.items, next_url=next_url, prev_url=prev_url)
     return render_template('disponibilidade.html', pagination=pagination)
 
-#@app.route('/add_data')
-#def add_data():
-#    return render_template('cadastrar.html')
-   
+# Função para deletar um Livro cadastrado na base de dados   
 @app.route('/delete/<int:id>')
 def erase(id):    
-    # letes the data on the basis of unique id and 
-    # directs to hoem page
     data = Catalogoslivros.query.get(id)
     db.session.delete(data)
     db.session.commit()
+    db.session.close()
     return redirect('/disponibilidade')
 
-#Pagina para informações do aplicativo
+#Funçaõ utilizada para pesquisar Titulo ou Nome do Autor do Livro 
 @app.route("/pesquisar", methods=['GET','POST'])
 def pesquisar():
     form = LivroForm()
     select = request.form.get('opcoes')
+    if request.method == 'POST':
+        #Pesquisa por Titulo do Livro
+        if select == 'titulo':
+            titulo_pesq = request.form.get("titulo_pesq")
+            search = "%{}%".format(titulo_pesq)
+            if titulo_pesq is not None:
+                data = Catalogoslivros.query.filter(Catalogoslivros.titulo.like(search)).all()
+                page = request.args.get('page', 1, type=int)
+                pagination = Catalogoslivros.query.filter(Catalogoslivros.titulo.like(search)).paginate(page=page, per_page=5)
+                return render_template("resultado.html", data=data, pagination=pagination)
+            else:
+                return render_template("pesquisar.html", form=form, select=select)    
+        #Pesquisa por Autor do Livro
+        if select == 'autor':
+            autor_pesq = request.form.get("autor_pesq")
+            search = "%{}%".format(autor_pesq)
+            if autor_pesq is not None:
+                data = Catalogoslivros.query.filter(Catalogoslivros.autor.like(search)).all()
+                page = request.args.get('page', 1, type=int)
+                pagination = Catalogoslivros.query.filter(Catalogoslivros.autor.like(search)).paginate(page=page, per_page=5)
+                return render_template("resultado.html", data=data, pagination=pagination)
+            else:
+                    return render_template("pesquisar.html", form=form, select=select)    
+
     return render_template("pesquisar.html", form=form, select=select)
 
 if __name__ == '__main__':
